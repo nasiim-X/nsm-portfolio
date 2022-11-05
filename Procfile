b2a08df5ebc7a6efb8c93dbb6fe2aa2c38195cf9
Nasiim X <nasiim.nsmlp@gmail.com>
2022-11-05 15:00:39 +0600

    ready to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nsm-portfolio.wsgi --log-file -
--- nsm_resume/settings.py
+++ nsm_resume/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)9&$z+zo0hpg5o*6rq$ows_n3_!zl$ycr+p76w5fy0ma5i)m0k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nsm-portfolio.herokuapp.com']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
